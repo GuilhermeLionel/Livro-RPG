10937b962d29c631f3890d91f94eb2480c60af1f
Lionel <4542.lionel@mail.com>
2025-05-11 21:10:21 -0300

    Atualização cabeçalho e integrantes

--- codigoFonte.c
+++ codigoFonte.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
 void limparTerminal(){
     printf("\033[H\033[J");
 }
 
-void cabecaTela(char* x){
-    printf("----------------------\n");
-    printf("-------- %s --------\n", x);
-    printf("----------------------\n");
+void cabecaTela(char* x) {
+    int largura = 30; // Largura total da moldura
+    int textoLen = strlen(x);
+    int espacos = largura - textoLen - 2; // -2 por causa dos espaços antes e depois do texto
+    int antes = espacos / 2;
+    int depois = espacos - antes;
+
+    // Linha superior
+    for (int i = 0; i < largura; i++) printf("-");
+    printf("\n");
+
+    // Linha do texto centralizado
+    for (int i = 0; i < antes; i++) printf("-");
+    printf(" %s ", x);
+    for (int i = 0; i < depois; i++) printf("-");
+    printf("\n");
+
+    // Linha inferior
+    for (int i = 0; i < largura; i++) printf("-");
+    printf("\n");
 }
 
 void telaInicial(){
+    limparTerminal();
     cabecaTela("jogo");
     printf("1 - Iniciar um novo jogo.\n");
     printf("2 - Continuar um jogo gravado.\n");
@@ -40,6 +58,23 @@ void telaInicial(){
     case 4:
         limparTerminal();
         cabecaTela("Interantes");
+        printf("\n\033[31mGuilherme Lionel de Souza\033[0m\n");
+        printf("\n\033[34mAlexandre Gabriel Angelo de Souza Blandino\033[0m\n");
+        printf("\n\033[32mSamuel Pereira da Silva\n\033[0m\n");
+
+        printf("[1] - Voltar para a tela inicial.\n");
+
+        while (1)
+        {
+            scanf("%d", &n);
+            if (n==1)
+            {
+                break;
+            }
+        }
+        telaInicial();
+        
+        
         break;
 
     default:
