5e4a1e02e514f6ab4415c555a000e8f14fb73afc
Samuel Silva <samuel110407@gmail.com>
2025-06-30 13:43:45 -0300

    melhorias na função atualizaRanking

--- codigoFonte.c
+++ codigoFonte.c
@@ -188,28 +188,27 @@ void ranking()
     fclose(fp);
 }
 
-void atualizaRanking(char *nomeArquivo, RANKING player) { // cria um novo arquivo de ranking (tentar sobrescrever o arquivo original depois)
+
+void atualizaRanking(char *nomeArquivo, RANKING player) {
     RANKING playerRanking[8]; // vetor de structs dos jogadores do ranking atual
-    FILE *ranking = fopen(nomeArquivo, "r"), *novoRanking = fopen("novoRanking.txt", "w");
-    int posicao, posicaoPlayer, colocacao, i = 0, OK = 1; // OK - verifica se o player já foi inserido no ranking
+    FILE *ranking = fopen(nomeArquivo, "r+");
+    int posicao, i = 0, j;
     while(!feof(ranking)) {
-        fscanf(ranking, "%d - %s - lvl: %d - %d\n", &posicao, playerRanking[i].nome, &playerRanking[i].level, &playerRanking[i].pontuacao); // lê os players do ranking
-        if (playerRanking[i].pontuacao > player.pontuacao) 
-            fprintf(novoRanking, "%d - %s - lvl: %d - %d\n", posicao, playerRanking[i].nome, playerRanking[i].level, playerRanking[i].pontuacao); // imprime os jogadores à frente do player
-        else { 
-            if (OK) {
-                posicaoPlayer = i;
-                fprintf(novoRanking, "%d - %s - lvl: %d - %d\n", posicao, player.nome, player.level, player.pontuacao); // imprime o player na sua colocação
-                OK = 0; // player está no ranking
-            }
+        fscanf(ranking, "%d - %s - lvl: %d - %d", &posicao, playerRanking[i].nome, &playerRanking[i].level, &playerRanking[i].pontuacao); // lê os players do ranking
+        i++;    
+    }
+    for (i = 0; i < 8; i++) 
+        if (player.pontuacao > playerRanking[i].pontuacao) { // reordenação do vetor de players
+            for (j = 7; j > i; j--) 
+                playerRanking[j] = playerRanking[j - 1];
+            playerRanking[i] = player;
+            break;
         }
-        i++;
-    } 
-    for (i = posicaoPlayer, colocacao = posicaoPlayer + 2; colocacao <= 8; i++, colocacao++) // imprime os players restantes, começando na próxima colocação e terminando na 8ª
-    // i = posicaoPlayer: pega o índice para o playerRanking
-        fprintf(novoRanking, "%d - %s - lvl: %d - %d\n", colocacao, playerRanking[i].nome, playerRanking[i].level, playerRanking[i].pontuacao);
+    rewind(ranking);
+    for (i = 0; i < 8; i++) { // imprime os players no arquivo
+        fprintf(ranking, "%d - %s - lvl: %d - %d\n", i + 1, playerRanking[i].nome, playerRanking[i].level, playerRanking[i].pontuacao);
+    }
     fclose(ranking);
-    fclose(novoRanking);
 }
 
 int loja0[3], checkLoja0 = 1;
